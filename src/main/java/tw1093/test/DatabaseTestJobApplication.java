1b0c8a5614248359779c08676aa9d2b5ddc62b81
Albert Lee <albertlee@melaleuca.com>
2017-09-07 16:41:24 +0800

    initial commit

---
+++ src/main/java/tw1093/test/DatabaseTestJobApplication.java
@@ -0,0 +1,12 @@
+package tw1093.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseTestJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseTestJobApplication.class, args);
+	}
+}
---
+++ src/test/java/tw1093/test/DatabaseTestJobApplicationTests.java
@@ -0,0 +1,16 @@
+package tw1093.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DatabaseTestJobApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
